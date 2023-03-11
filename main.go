de295dfbe944f96708e22845af1b2c639a66333e
SouJhin <soujhin2022@gmail.com>
2023-03-11 16:10:46 +0800

    database.go

--- main.go
+++ main.go
@@ -21,10 +21,28 @@ func initDB() (err error) {
 	return
 }
 
+type user struct {
+	Id   int    `db:"id"`
+	Name string `db:"name"`
+	Age  int    `db:"age"`
+}
+
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u user
+	err := db.Get(&u, sqlStr, 1)
+	if err != nil {
+		fmt.Printf("err =====> 🚀🚀🚀 %v\n", err)
+		return
+	}
+	fmt.Printf("u =====> 🚀🚀🚀 %v\n %v\n %v\n ", u.Name, u.Age, u.Id)
+}
+
 func main() {
 	if err := initDB(); err != nil {
 		fmt.Printf("err =====> 🚀🚀🚀 %v\n", err)
 		return
 	}
-	fmt.Printf("success =====> 🚀🚀🚀 %v\n")
+	fmt.Printf("success =====> 🚀🚀🚀")
+	queryRowDemo()
 }
